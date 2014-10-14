24535dfbdaa7f2a8003266af421e6700ae6b85f2
Evgeniy Plyusnin <eninbox@gmail.com>
2014-10-14 13:49:40 +0600

    Чуть переработан IRepository

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -2,8 +2,26 @@ using System.Collections.Generic;
 
 namespace FirmwarePacking.Repositories
 {
+    /// <summary>Интерфейс репозитория -- хранилища прошивок</summary>
     public interface IRepository
     {
+        /// <summary>Список всех пакетов в репозитории</summary>
         IList<FirmwarePackage> Packages { get; }
+
+        /// <summary>Находит пакет прошивки, содержащий компоненты для всех указанных целей</summary>
+        /// <param name="Targets">Цели прошивки</param>
+        IEnumerable<FirmwarePackage> GetPackagesForTargets(IList<ComponentTarget> Targets);
+    }
+
+    /// <summary>Содержит методы расширения для работы с репозиторием</summary>
+    public static class RepositoryHelper
+    {
+        /// <summary>Находит пакет прошивки, содержащий компоненты для всех указанных целей</summary>
+        /// <param name="Repository">Репозиторий для поиска</param>
+        /// <param name="Targets">Цели прошивки</param>
+        public static IEnumerable<FirmwarePackage> GetPackagesForTargets(this IRepository Repository, params ComponentTarget[] Targets)
+        {
+            return Repository.GetPackagesForTargets(Targets);
+        }
     }
 }
--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace FirmwarePacking.Repositories
 {
-    /// <summary>
-    /// Представляет абстрактную модель репозитория с прошивками
-    /// </summary>
+    /// <summary>Представляет абстрактную модель репозитория с прошивками</summary>
     public abstract class Repository : IRepository
     {
+        /// <summary>Список всех пакетов в репозитории</summary>
         public abstract IList<FirmwarePackage> Packages { get; }
 
-        /// <summary>
-        /// Находит пакет прошивки, содержащий компоненты для всех указанных целей
-        /// </summary>
-        /// <param name="Targets">Цели прошивки</param>
-        public IEnumerable<FirmwarePackage> GetPackagesForTargets(params ComponentTarget[] Targets) { return GetPackagesForTargets((IList<ComponentTarget>)Targets); }
-        /// <summary>
-        /// Находит пакет прошивки, содержащий компоненты для всех указанных целей
-        /// </summary>
+        /// <summary>Находит пакет прошивки, содержащий компоненты для всех указанных целей</summary>
         /// <param name="Targets">Цели прошивки</param>
         public virtual IEnumerable<FirmwarePackage> GetPackagesForTargets(IList<ComponentTarget> Targets)
         {
