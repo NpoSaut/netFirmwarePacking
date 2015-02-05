e1f5f87d096cffe0274792ba88ea79b365670142
Evgeniy Plyusnin <eninbox@gmail.com>
2015-02-05 11:26:51 +0500

    Какой-то первый тест, который что-то проверил

---
+++ FirmwarePacking.Repositories.Remote.Test/Functional/ServerInteractionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel;
+using FirmwarePacking.Repositories.Remote.SautRepoApi;
+using NUnit.Framework;
+
+namespace FirmwarePacking.Repositories.Remote.Test.Functional
+{
+    [TestFixture]
+    public class ServerInteractionsTests
+    {
+        private static SfpRepositoryServiceClient CreateClient()
+        {
+            return new SfpRepositoryServiceClient(new BasicHttpBinding(), new EndpointAddress("http://repo/api/SfpRepositoryService.svc"));
+        }
+
+        [Test]
+        [Description("Проверка возможности подключиться")]
+        public void ConnectivityTest()
+        {
+            using (SfpRepositoryServiceClient client = CreateClient())
+            {
+                client.Open();
+                Assert.AreEqual(client.State, CommunicationState.Opened);
+            }
+        }
+
+        [Test]
+        [Description("Загрузка списка пакетов для БС-ДПС")]
+        public void LoadSomeDataTest()
+        {
+            using (SfpRepositoryServiceClient client = CreateClient())
+            {
+                PackageDescription[] packageDescriptions = client.GetCompositeTypes(20, 1);
+                CollectionAssert.IsNotEmpty(packageDescriptions);
+            }
+        }
+    }
+}
