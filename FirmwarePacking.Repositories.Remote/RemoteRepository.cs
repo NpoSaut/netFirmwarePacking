0faf0a71e9293b468a2f5ebf5375ea38e051cee5
Evgeniy Plyusnin <eninbox@gmail.com>
2015-02-05 11:22:38 +0500

    Первая черновая версия, которая смогла получить какие-то данные с сервера

---
+++ FirmwarePacking.Repositories.Remote/RemoteRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using FirmwarePacking.Repositories.Remote.SautRepoApi;
+
+namespace FirmwarePacking.Repositories.Remote
+{
+    public class RemoteRepository : IRepository
+    {
+        private ISfpRepositoryService _repositoryService;
+
+        public RemoteRepository() { _repositoryService = new SfpRepositoryServiceClient(); }
+
+        /// <summary>Список всех пакетов в репозитории</summary>
+        public IEnumerable<IRepositoryElement> Packages { get; private set; }
+
+        /// <summary>Находит пакет прошивки, содержащий компоненты для всех указанных целей</summary>
+        /// <param name="Targets">Цели прошивки</param>
+        public IEnumerable<IRepositoryElement> GetPackagesForTargets(ICollection<ComponentTarget> Targets) { throw new NotImplementedException(); }
+    }
+}
