33411545fe123118f0a1a7159433ec78d25dcd5b
Evgeniy Plyusnin <eninbox@gmail.com>
2015-02-16 19:15:21 +0500

    Из IRepositoryElement вынесен интерфейс IFirmwarePackageProvider

--- Repositories/IRepositoryElement.cs
+++ Repositories/IRepositoryElement.cs
@@ -18,12 +18,18 @@ namespace FirmwarePacking.Repositories
         Archive
     }
 
+    public interface IFirmwarePackageProvider
+    {
+        /// <summary>Загружает всё тело пакета</summary>
+        FirmwarePackage GetPackage();
+    }
+
     /// <summary>Элемент репозитория.</summary>
     /// <remarks>
     ///     Ставится в соответствие пакету прошивки в репозитории и позволяет получить информацию о нём, не загружая его
     ///     тело, либо частично загрузить один из компонентов пакета.
     /// </remarks>
-    public interface IRepositoryElement
+    public interface IRepositoryElement : IFirmwarePackageProvider
     {
         /// <summary>Статус релиза пакета</summary>
         ReleaseStatus Status { get; }
@@ -33,9 +39,6 @@ namespace FirmwarePacking.Repositories
 
         /// <summary>Список целей, для которых имеются компоненты в данном пакете</summary>
         ICollection<ComponentTarget> Targets { get; }
-
-        /// <summary>Загружает всё тело пакета</summary>
-        FirmwarePackage GetPackage();
 
         /// <summary>Загружает необходимый компонент из тела пакета</summary>
         /// <param name="Target">Цель, компонент для которой требуется</param>
