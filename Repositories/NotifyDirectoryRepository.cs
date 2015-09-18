205f8b61794b5419d45150101f6669e590c33df0
Evgeniy Plyusnin <eninbox@gmail.com>
2015-09-18 14:13:29 +0500

    Прокачал NotifyDirectoryRepository, теперь он асинхронный и дожидается единоличного права на открытие файла. Это позволяет корректно обрабатывать файлы, скачанные в браузере.

--- Repositories/NotifyDirectoryRepository.cs
+++ Repositories/NotifyDirectoryRepository.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FirmwarePacking.Repositories
 {
     public class NotifyDirectoryRepository : DirectoryRepository, INotifyRepository
     {
+        private readonly ConcurrentDictionary<string, CancellationTokenSource> _fileWatchOperations =
+            new ConcurrentDictionary<string, CancellationTokenSource>();
+
         private readonly object _locker = new object();
         private readonly FileSystemWatcher _watcher;
 
@@ -29,43 +35,80 @@ namespace FirmwarePacking.Repositories
             _watcher.EnableRaisingEvents = true;
         }
 
+        private void WatcherOnCreated(object Sender, FileSystemEventArgs e) { ProcessFileCreated(e.FullPath); }
+        private void WatcherOnDeleted(object Sender, FileSystemEventArgs e) { ProcessFileRemoved(e.FullPath); }
+
         private void WatcherOnRenamed(object Sender, RenamedEventArgs e)
         {
-            lock (_locker)
-            {
-                IRepositoryElement element;
-                if (ElementsCache.TryGetValue(e.OldFullPath, out element))
-                {
-                    ElementsCache.Add(e.FullPath, element);
-                    ElementsCache.Remove(e.OldFullPath);
-                }
-            }
+            if (e.OldFullPath != null
+                && Path.GetExtension(e.OldFullPath).Equals("." + FirmwarePackage.FirmwarePackageExtension, StringComparison.CurrentCultureIgnoreCase))
+                ProcessFileRemoved(e.OldFullPath);
+            if (e.FullPath != null
+                && Path.GetExtension(e.FullPath).Equals("." + FirmwarePackage.FirmwarePackageExtension, StringComparison.CurrentCultureIgnoreCase))
+                ProcessFileCreated(e.FullPath);
         }
 
-        private void WatcherOnCreated(object Sender, FileSystemEventArgs e)
+        private void ProcessFileCreated(string FileName)
         {
-            IRepositoryElement element = LoadElement(e.FullPath);
-            lock (_locker)
-            {
-                ElementsCache.Add(e.FullPath, element);
-                PackagesCollection.Add(element);
-            }
-            OnUpdated(RepositoryUpdatedEventArgs.CreateAddedEventArgs(element));
+            var ts = new CancellationTokenSource();
+            _fileWatchOperations.TryAdd(FileName, ts);
+            CancellationToken token = ts.Token;
+            Task.Factory.StartNew(() =>
+                                  {
+                                      try
+                                      {
+                                          SpinWait.SpinUntil(() =>
+                                                             {
+                                                                 token.ThrowIfCancellationRequested();
+                                                                 return TestFile(FileName);
+                                                             });
+                                          IRepositoryElement element = LoadElement(FileName);
+                                          lock (_locker)
+                                          {
+                                              ElementsCache.Add(FileName, element);
+                                              PackagesCollection.Add(element);
+                                          }
+                                          OnUpdated(RepositoryUpdatedEventArgs.CreateAddedEventArgs(element));
+                                      }
+                                      catch (OperationCanceledException e) { }
+                                      finally
+                                      {
+                                          _fileWatchOperations.TryRemove(FileName, out ts);
+                                      }
+                                  }, token);
         }
 
-        private void WatcherOnDeleted(object Sender, FileSystemEventArgs e)
+        private void ProcessFileRemoved(string FullPath)
         {
+            CancellationTokenSource ts;
+            if (_fileWatchOperations.TryRemove(FullPath, out ts))
+                ts.Cancel();
             IRepositoryElement element;
             lock (_locker)
             {
-                if (ElementsCache.TryGetValue(e.FullPath, out element))
+                if (ElementsCache.TryGetValue(FullPath, out element))
                 {
-                    ElementsCache.Remove(e.FullPath);
+                    ElementsCache.Remove(FullPath);
                     PackagesCollection.Remove(element);
                 }
             }
             if (element != null)
                 OnUpdated(RepositoryUpdatedEventArgs.CreateRemovedEventArgs(element));
+        }
+
+        private bool TestFile(String FileName)
+        {
+            try
+            {
+                using (File.Open(FileName, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         protected virtual void OnUpdated(RepositoryUpdatedEventArgs E)
