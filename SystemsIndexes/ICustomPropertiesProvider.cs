3159e051c10caeda56915b8ff05c0edc4763cf7f
Evgeniy Plyusnin <eninbox@gmail.com>
2018-01-29 14:31:39 +0500

    Описание исключения я ICustomPropertiesProvider

--- SystemsIndexes/ICustomPropertiesProvider.cs
+++ SystemsIndexes/ICustomPropertiesProvider.cs
@@ -1,9 +1,13 @@
 ﻿using FirmwarePacking.Annotations;
+using FirmwarePacking.SystemsIndexes.Exceptions;
 
 namespace FirmwarePacking.SystemsIndexes
 {
     public interface ICustomPropertiesProvider
     {
+        /// <summary>Возвращает значение пользовательского свойства или выбрасывает исключение, если свойство не найдено</summary>
+        /// <param name="PropertyName">Имя свойства</param>
+        /// <exception cref="CustomPropertyIsNotSpecifiedIndexException">Свойство с таким именем не указано для данной ячейки</exception>
         [NotNull]
         string this[string PropertyName] { get; }
 
