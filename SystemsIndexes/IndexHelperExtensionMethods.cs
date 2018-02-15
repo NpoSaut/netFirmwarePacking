23d5d9baa9df039d6646db24f756099a6e063b62
Evgeniy Plyusnin <eninbox@gmail.com>
2018-02-15 17:12:27 +0500

    Добавил класс IndexHelperExtensionMethods, в который добавил TryGetCell(), который может вернуть null, если не найдёт ячейку

---
+++ SystemsIndexes/IndexHelperExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using FirmwarePacking.Annotations;
+using FirmwarePacking.SystemsIndexes.Exceptions;
+
+namespace FirmwarePacking.SystemsIndexes
+{
+    public static class IndexHelperExtensionMethods
+    {
+        [CanBeNull]
+        public static BlockKind TryGetCell([NotNull] this IIndexHelper IndexHelper, int CellId)
+        {
+            try
+            {
+                return IndexHelper.GetCell(CellId);
+            }
+            catch (IndexException)
+            {
+                return null;
+            }
+        }
+    }
+}
